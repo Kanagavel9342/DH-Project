7fc9a60aa9bb48529d3558aab897f30a70480092
kanagavel <skvel.fx@gmail.com>
2025-05-16 22:27:52 +0530

    Deploying

--- server/index.js
+++ server/index.js
@@ -378,7 +378,7 @@ app.use((err, req, res, next) => {
 });
 
 // ---------- Start Server ----------
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
